e0c976ca2db9b6fd9fa1a4bf3cd231090a101bf4
skophek <skophek@lipe.cz>
2022-05-31 17:08:34 +0200

    character attributes output - attrs can be modified by equipment

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -2,36 +2,57 @@ import React, { useContext } from "react";
 import Ctx from "../../Ctx";
 import Equipment from "../Equipment/Equipment";
 import "./Character.scss";
-import { CharacterModelProp, EQUIPMENT_KEY } from "./CharacterModel";
+import CharacterModel, { characterAttrModifiers, CharacterAttrs, CharacterModelProp, EQUIPMENT_KEY } from "./CharacterModel";
 
 const Character = () => {
   const {character} = useContext(Ctx);
+  const charInfo: Partial<{[k in CharacterModelProp]: string | JSX.Element}> = {};
 
-  let equipmentList: string | JSX.Element = 'No equipment';
-  if (character && character[EQUIPMENT_KEY].length > 0)
+  if (character)
   {
-    equipmentList = (
-      <ul>
-        {character[EQUIPMENT_KEY].map((equipmentItem, i) => <li key={i}><Equipment item={equipmentItem}></Equipment></li>)}
-      </ul>
-    );
+    // evaluate each attribute
+    (Object.keys(character) as (keyof CharacterModel)[]).forEach(key => {
+      const modifier = characterAttrModifiers[key as CharacterAttrs];
+      // attribute can be modified
+      if (modifier)
+      {
+        charInfo[key as keyof typeof characterAttrModifiers] = modifier(character[key as CharacterAttrs], character).toString();
+      }
+      // prepare list of equipment
+      else if (key === EQUIPMENT_KEY)
+      {
+        let equipmentList: string | JSX.Element = 'No equipment';
+        if (character && character[key].length > 0)
+        {
+          equipmentList = (
+            <ul>
+              {character[EQUIPMENT_KEY].map((equipmentItem, i) => <li key={i}><Equipment item={equipmentItem}></Equipment></li>)}
+            </ul>
+          );
+        }
+        charInfo[key] = equipmentList;
+      }
+      else
+      {
+        charInfo[key] = character[key].toString();
+      }
+    });
   }
+
+  const charInfoKeys = Object.keys(charInfo);
 
   return (
     <div className="Character">
-      <dl>
-      {character && Object.keys(character).map((key) => (
-        <React.Fragment key={key}>
-          <dt>{key[0].toUpperCase() + key.slice(1)}</dt>
-          <dd>
-            <>
-              {key !== EQUIPMENT_KEY && character[key as CharacterModelProp]}
-              {key === EQUIPMENT_KEY && equipmentList}
-            </>
-          </dd>
-        </React.Fragment>
-      ))}
-      </dl>
+      {charInfoKeys.length > 0 && (
+        <dl>
+        {charInfoKeys.map((key) => (
+          <React.Fragment key={key}>
+            <dt>{key[0].toUpperCase() + key.slice(1)}</dt>
+            <dd>{charInfo[key as CharacterModelProp]}</dd>
+          </React.Fragment>
+        ))}
+        </dl>
+      )}
     </div>
   );
 };
--- src/components/Character/CharacterModel.tsx
+++ src/components/Character/CharacterModel.tsx
@@ -10,8 +10,21 @@ interface CharacterModel {
     [EQUIPMENT_KEY]: EquipmentModel[];
 }
 
+const characterAttrModifiers = {
+    hitPoints: function modify(hp: number, character: CharacterModel): number {
+        const equipment = character[EQUIPMENT_KEY];
+        return hp + equipment.reduce((hpModSum, item) => hpModSum + item.hpModifier, 0);
+    },
+    luck: function modify(luck: number, character: CharacterModel): number {
+        const equipment = character[EQUIPMENT_KEY];
+        return luck + equipment.reduce((luckModSum, item) => luckModSum + item.luckModifier, 0);
+    },
+}
+
+type CharacterAttrs = keyof typeof characterAttrModifiers;
 type CharacterModelProp = keyof CharacterModel;
 
 export default CharacterModel;
-export type { CharacterModelProp };
+export { characterAttrModifiers };
+export type { CharacterAttrs, CharacterModelProp };
 export { EQUIPMENT_KEY };
