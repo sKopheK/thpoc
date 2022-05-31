64c087b0c32cdb75b96c381cb98efa93eb92109e
skophek <skophek@lipe.cz>
2022-05-31 13:15:06 +0200

    API request for character details - no longer outputs in console

--- src/apiRequests.ts
+++ src/apiRequests.ts
@@ -4,7 +4,6 @@ import CharacterModel from "./components/Character/CharacterModel";
 async function getCharacter(): Promise<CharacterModel> {
   const url = "/api/character";
   const resp = await axios.get<CharacterModel>(url);
-  console.log(resp);
   return resp.data;
 }
 
